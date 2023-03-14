279b3c43d5a9e90957e246cea146f32105a31ebe
Gaston Avila <gastonavila14@gmail.com>
2023-03-13 21:08:18 -0300

    Clase 2.3 - Bases React - useState Hook - Task App List - 11/03/23

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 // Functional component O Componente basado en funcion
-
 import { useState } from "react"
-
+import style from "./App.module.css"
 // PROPS => Properties
 export function App({tema, fecha}) {
-
-  const COUNTER_DEFAULT = 10
-  const [counter, setCounter] = useState(COUNTER_DEFAULT)
+  const [taskList, setTaskList] = useState([])
+  const [taskPreview, setTaskPreview] = useState("")
   return (
     <>
-        <h1>Tema: {tema} - Fecha: {fecha}</h1>
-        <h4>Counter APP</h4>
-        <h2>Contador: {counter}</h2>
-        <button onClick={()=> setCounter( counter + 1 ) }>+1</button>
-        <button onClick={ ()=>setCounter( counter - 1 ) }>-1</button>
-        <button onClick={ ()=> setCounter(COUNTER_DEFAULT) }>Reset</button>
+        <h1 className={style.title}>Tema: {tema} - Fecha: {fecha}</h1>
+        <h2>TODO list (Listado de tarea)</h2>
+        <input placeholder="Ingresa tarea" onKeyUp={(e)=>{
+          const userInput = e.target.value
+          setTaskPreview(userInput) // la entrada de texto del usuario
+          if(e.code === "Enter"){
+            setTaskList([ ...taskList, userInput])
+            setTaskPreview("")
+            e.target.value = ""
+          }
+
+        }}/>
+        <p>Tarea a ingresar: {taskPreview}</p>
+        <ul>
+          {
+            taskList.map( (task,i) =>{
+              return <li key={i} className={style.listItem}>{task}</li>
+            })
+          } 
+        </ul>
     </>
   )  // jsx
 }
