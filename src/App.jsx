bf792a736bded68881f6c7c6a4404a566a767374
Gaston Avila <gastonavila14@gmail.com>
2023-04-26 21:01:16 -0300

    Ejemplo de ruta condicional

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ import { ProductsPage } from "./Pages/ProductsPage";
 import { LoginPage } from "./Pages/LoginPage";
 import { RegisterPage } from "./Pages/RegisterPage";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8085/api";
-// repo-hello-express-rollingclases-production.up.railway.app/api
+// import { useEffect } from "react";
+axios.defaults.baseURL = process.env.REACT_API_URL || "https://repo-hello-express-rollingclases-production.up.railway.app/api";
 
 export function App() {
+  // const [user, setUser] = useState(null); // {id: 1, name: "Juan", email: "juan@juan", role: "admin"}
+  
+  // useEffect(() => {
+  //   axios.get("/auth/me").then((res) => {
+  //     setUser(res.data);
+  //   });
+  // }, []);
   return (
     <ChakraProvider>
       <BrowserRouter>
@@ -21,7 +28,8 @@ export function App() {
             <Route path="" element={<SiteUnderContruction/>} />
             <Route path="productos" element={<ProductsPage/>}/>
             <Route path="ventas" element={<SiteUnderContruction />} />
-            <Route path="*" element={<SiteUnderContruction />} />
+            {/* <Route path="admin" element={ user.role === "admin" && <AdminPage /> : <SiteUnderContruction/>} /> */}
+            <Route path="*" element={<NoPage />} />
           </Route>
           <Route path="*" element={<NoPage />} />
         </Routes>
